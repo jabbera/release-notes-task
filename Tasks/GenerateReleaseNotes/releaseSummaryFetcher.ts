bcdfd54548f3cd564cbcabe9afe8370e895ab172
Mike <github@barry.io>
2017-03-14 11:09:54 -0400

    Get Git changes

--- Tasks/GenerateReleaseNotes/releaseSummaryFetcher.ts
+++ Tasks/GenerateReleaseNotes/releaseSummaryFetcher.ts
@@ -1,7 +1,10 @@
 import * as vsts from "vso-node-api";
+import * as bi from "vso-node-api/BuildApi";
+import * as git from "vso-node-api/GitApi";
 import * as ra from "vso-node-api/ReleaseApi";
 import * as tfvc from "vso-node-api/TfvcApi";
 import * as wa from "vso-node-api/WorkItemTrackingApi";
+import * as giti from "vso-node-api/interfaces/GitInterfaces";
 import * as ri from "vso-node-api/interfaces/ReleaseInterfaces";
 import * as tfcvi from "vso-node-api/interfaces/TfvcInterfaces";
 import * as wi from "vso-node-api/interfaces/WorkItemTrackingInterfaces";
@@ -64,8 +67,9 @@ export class ReleaseSummaryFetcher implements IReleaseSummaryFetcher {
         let token: string = tl.getEndpointAuthorizationParameter("SystemVssConnection", "AccessToken", false);
         let collectionUrl: string = tl.getEndpointUrl("SystemVssConnection", false);
         let authHandler = vsts.getPersonalAccessTokenHandler(token);
-        let connect = new vsts.WebApi(collectionUrl, authHandler);
-        let releaseApi: ra.IReleaseApi = connect.getReleaseApi();
+        let rconnect = new vsts.WebApi(collectionUrl, authHandler);
+        let connect = new vsts.WebApi(collectionUrl.replace("vsrm.", ""), authHandler);
+        let releaseApi: ra.IReleaseApi = rconnect.getReleaseApi();
 
         let releases: ri.Release[] = await releaseApi.getReleases(this.teamProject, this.releaseDefinitionId,
             this.definitionEnvironmentId, null, null, null, ri.EnvironmentStatus.Succeeded | ri.EnvironmentStatus.InProgress,
@@ -83,7 +87,7 @@ export class ReleaseSummaryFetcher implements IReleaseSummaryFetcher {
         // The documentation is backwards on the releaseid versus the basereleaseid
         let rawChanges: ri.Change[] = await releaseApi.getReleaseChanges(this.teamProject, this.currentlyDeployedReleaseField.id, this.releaseInProgressField.id, 1000);
 
-        this.changesInReleaseField = await this.getTfvsChanges(rawChanges, connect);
+        this.changesInReleaseField = await this.getChanges(rawChanges, connect);
 
         this.isInited = true;
     }
@@ -99,13 +103,65 @@ export class ReleaseSummaryFetcher implements IReleaseSummaryFetcher {
         return {buildVersion: buildVersion, buildWebLink: buildWebLink, releaseWebLink: releasWebLink, definitionName: definitionName, environmentName: environmentName};
     }
 
-    private async getTfvsChanges(changesInRelease: ri.Change[], connect: vsts.WebApi): Promise<ReleaseCommit[]> {
+    private async getChanges(changesInRelease: ri.Change[], connect: vsts.WebApi): Promise<ReleaseCommit[]> {
+        let tfvcChanges: Promise<ReleaseCommit[]> = this.getTfvcChanges(changesInRelease, connect);
+        let gitChanges: Promise<ReleaseCommit[]> = this.getGitChanges(changesInRelease, connect);
+
+        return (await tfvcChanges).concat(await gitChanges);
+    }
+
+    private async getGitChanges(changesInRelease: ri.Change[], connect: vsts.WebApi): Promise<ReleaseCommit[]> {
+        let api: git.IGitApi = connect.getGitApi();
+        let idsToGet: string[] = changesInRelease.filter((x) => x.changeType === "TfsGit").map((x) => x.id);
+
+        if (idsToGet.length === 0) {
+            return [];
+        }
+
+        let query: giti.GitQueryCommitsCriteria = {
+            "$top": 1000,
+            "itemPath": "",
+                "itemVersion": {
+                    "version": idsToGet[idsToGet.length - 1],
+                    "versionType": giti.GitVersionType.Commit,
+                    "versionOptions": giti.GitVersionOptions.None,
+                },
+                "compareVersion": {
+                    "version": idsToGet[0],
+                    "versionType": giti.GitVersionType.Commit,
+                    "versionOptions": giti.GitVersionOptions.None,
+                },
+        } as giti.GitQueryCommitsCriteria;
+
+        let gitChanges: giti.GitCommitRef[] = await api.getCommitsBatch(query, this.teamProject, this.teamProject);
+        if (gitChanges == null || gitChanges.length === 0) {
+            throw new Error("No changesets returns from API");
+        }
+
+        let returnValue: ReleaseCommit[] = gitChanges.map((x) => {
+            return {
+                "author": x.author.name,
+                "id": x.commitId.substring(0, 8),
+                "link": x.remoteUrl,
+                "message": x.comment,
+                "timestamp": x.author.date,
+            };
+        });
+
+        return returnValue;
+    }
+
+    private async getTfvcChanges(changesInRelease: ri.Change[], connect: vsts.WebApi): Promise<ReleaseCommit[]> {
         let api: tfvc.ITfvcApi = connect.getTfvcApi();
-        let idsToGet:number[] = changesInRelease.filter((x) => x.changeType === "TfsVersionControl").map((x) => Number(x.id.replace("C", "")));
+        let idsToGet: number[] = changesInRelease.filter((x) => x.changeType === "TfsVersionControl").map((x) => Number(x.id.replace("C", "")));
+
+        if (idsToGet.length === 0) {
+            return [];
+        }
 
         let tfvcChanges: tfcvi.TfvcChangesetRef[] = await api.getBatchedChangesets({ "changesetIds": idsToGet, "commentLength": 100, "includeLinks": true});
         if (tfvcChanges == null || tfvcChanges.length === 0) {
-            return [];
+            throw new Error("No changesets returns from API");
         }
 
         let returnValue: ReleaseCommit[] = tfvcChanges.map((x) => {
